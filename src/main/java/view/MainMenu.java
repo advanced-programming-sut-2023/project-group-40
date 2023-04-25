2a1258bdd9d9dc3d4489c132332f23676729636d
artin <a2014.barghi@gmail.com>
2023-04-25 21:00:13 +0430

    main menu merge into phase1

--- src/main/java/view/MainMenu.java
+++ src/main/java/view/MainMenu.java
@@ -1,18 +1,40 @@
 package view;
 
+import controller.GameMenuController;
+import controller.MainController;
+import controller.MainMenuController;
+import controller.ProfileMenuController;
+import model.Government;
+import view.enums.Commands;
+
 import java.util.Scanner;
 import java.util.regex.Matcher;
 
 public class MainMenu {
-    public static void run(){
+    public static void run(Scanner scanner) throws ReflectiveOperationException {
+        while (true){
+            String command = scanner.nextLine();
+            String result = Commands.regexFinder(command,MainMenu.class);
+            if (result != null) System.out.println(result);
+        }
     }
     public static String enterProfileMenu(Matcher matcher){
+        System.out.println("you are in profile menu!");
+        ProfileMenuController.setCurrentUser(MainMenuController.getCurrentUser());
+        ProfileMenu.run();
         return null;
     }
     public static String enterGameMenu(Matcher matcher){
+        System.out.println("you are in game menu!");
+        GameMenuController.setCurrentGovernment(
+                Government.getGovernmentByUser(MainMenuController.getCurrentUser()));
+        GameMenu.run();
         return null;
     }
-    public static String logout(Matcher matcher){
+    public static String logout(Matcher matcher) throws ReflectiveOperationException {
+        System.out.println("user logged out successfully!");
+        MainMenuController.setCurrentUser(null);
+        LoginMenu.run();
         return null;
     }
 }
