5af91f3581935d588ed48f97834f8442b3d2bb3d
Arvin <mr.arvin697@gmail.com>
2023-04-25 22:59:07 +0430

    profileMenu merge into phase1

--- src/main/java/view/ProfileMenu.java
+++ src/main/java/view/ProfileMenu.java
@@ -1,37 +1,59 @@
 package view;
 
+import controller.MainController;
+import controller.ProfileMenuController;
+import view.enums.Commands;
+
 import java.util.Scanner;
 import java.util.regex.Matcher;
 
 public class ProfileMenu {
-    public static void run(){
+    public static void run() throws ReflectiveOperationException {
+        while (true) {
+            String command = MainController.scanner.nextLine();
+            String result = Commands.regexFinder(command, ProfileMenu.class);
+            if (command != null) System.out.println(result);
+        }
     }
     public static String changeUsername(Matcher matcher){
-        return null;
+        String username = matcher.group("username");
+        return ProfileMenuController.changeUsername(username);
     }
     public static String changePassword(Matcher matcher){
-        return null;
+        String oldPass = matcher.group("oldPassword");
+        String newPass = matcher.group("newPassword");
+        return ProfileMenuController.changePassword(oldPass, newPass);
     }
     public static String changeNickname(Matcher matcher){
-        return null;
+        String nickname = matcher.group("nickname");
+        return ProfileMenuController.changeNickname(nickname);
     }
     public static String changeEmail(Matcher matcher){
-        return null;
+        String email = matcher.group("email");
+        return ProfileMenuController.changeEmail(email);
     }
 
     public static String changeSlogan(Matcher matcher){
-        return null;
+        String slogan = matcher.group("slogan");
+        return ProfileMenuController.changeSlogan(slogan);
     }
 
     public static String displayHighScore(Matcher matcher){
-        return null;
+        return ProfileMenuController.displayHighScore();
     }
 
     public static String displayRank(Matcher matcher){
-        return null;
+        return ProfileMenuController.displayRank();
+    }
+    public static String displaySlogan(Matcher matcher) {
+        return ProfileMenuController.displaySlogan();
     }
 
     public static String displayProfile(Matcher matcher){
-        return null;
+        return ProfileMenuController.displayProfile();
+    }
+
+    public static String removeSlogan(){
+        return ProfileMenuController.removeSlogan();
     }
 }
--- src/main/java/view/enums/Commands.java
+++ src/main/java/view/enums/Commands.java
@@ -6,7 +6,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum Commands {
-    ;
+    PROFILE_CHANGE_USERNAME("profile change -u (?<username>(\"[^\"]*\")|([^\"\\s]*))", ProfileMenu.class, "changeUsername"),
+    PROFILE_CHANGE_NICKNAME("profile change -n (?<nickname>(\"[^\"]*\")|([^\"\\s]*))", ProfileMenu.class, "changeNickname"),
+    PROFILE_CHANGE_EMAIL("profile change -e (?<email>(\"[^\"]*\")|([^\"\\s]*))", ProfileMenu.class, "changeEmail"),
+    PROFILE_CHANGE_SLOGAN("profile change -s (?<slogan>(\"[^\"]*\")|([^\"\\s]*))", ProfileMenu.class, "changeSlogan"),
+    PROFILE_CHANGE_PASSWORD("profile change -o (?<oldPassword>(\"[^\"]*\")|([^\"\\s]*)) -n (?<newPassword>(\"[^\"]*\")|([^\"\\s]*))", ProfileMenu.class, "changePassword"),
+    PROFILE_REMOVE_SLOGAN("profile remove slogan", ProfileMenu.class, "removeSlogan"),
+    PROFILE_DISPLAY_HIGHSCORE("profile display highscore", ProfileMenu.class, "displayHighscore"),
+    PROFILE_DISPLAY_RANK("profile display rank", ProfileMenu.class, "displayRank"),
+    PROFILE_DISPLAY_SLOGAN("profile display slogan", ProfileMenu.class, "displaySlogan"),
+    PROFILE_DISPLAY("profile display", ProfileMenu.class, "profileDisplay");
     private final String regex;
     private final String methodName;
     private final Class<?> menuClass;
